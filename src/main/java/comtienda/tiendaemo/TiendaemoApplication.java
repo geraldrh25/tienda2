38dc179217aab2fb423a049e593df81b1ad9fca6
Admin <Admin@Gerald>
2023-06-13 22:48:48 -0600

    semana5

---
+++ src/main/java/comtienda/tiendaemo/TiendaemoApplication.java
@@ -0,0 +1,13 @@
+package comtienda.tiendaemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comtienda/tiendaemo/TiendaemoApplicationTests.java
@@ -0,0 +1,13 @@
+package comtienda.tiendaemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
